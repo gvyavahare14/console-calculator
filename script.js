8bfa79a24e0bbf47b014097aacba67d6977504b1
ganesh prakash vyavahare <123353213+gvyavahare14@users.noreply.github.com>
2023-04-01 13:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// calculator------------------------
+const a = parseInt(prompt('please enter your num1'));
+const b = parseInt(prompt('please enter your num2'));
+const c = prompt('please enter your oprt');
+
+
+
+
+
+if ( c == '+') {
+
+ alert(a + b);
+  
+}
+if ( c == '-') {
+
+    alert(a - b);
+  
+}
+if ( c == '*') {
+
+    alert(a * b);
+  
+}
+if ( c == '/') {
+
+    alert(a / b);
+  
+}
